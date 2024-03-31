3b61ffe135133bbcc07d82018847d0557b0f4eca
geofholbrook <geof.holbrook@gmail.com>
2024-03-30 21:39:56 -0400

    working with sharp

--- src/saveTile.ts
+++ src/saveTile.ts
@@ -3,8 +3,13 @@ import { fetchTile } from './fetchTile';
 import path from 'path';
 
 export async function saveTile(x: number, y: number, zoom: number): Promise<string> {
-    const buffer = await fetchTile(x, y, zoom);
     const savePath = path.dirname(__dirname) + `/png/tile-${zoom}-${x}-${y}.png`;
+    
+    if (process.env.SKIP_FETCH === 'true') {
+        return savePath;
+    }
+
+    const buffer = await fetchTile(x, y, zoom);
     writeFileSync(savePath, buffer);
     return savePath;
 }
