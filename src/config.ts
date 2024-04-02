07381b6c6b880a0ba0adc7fc7ea695689e12c7f7
geofholbrook <geof.holbrook@gmail.com>
2024-04-01 21:07:50 -0400

    adds public dir

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@ export const config = {
     tomtomApiKey: process.env.TOMTOM_API_KEY,
     tileDirectory: process.env.TILE_DIRECTORY,
     compositeDirectory: process.env.COMPOSITE_DIRECTORY!,
+    publicDirectory: process.env.PUBLIC_DIRECTORY!,
 };
--- src/server/index.ts
+++ src/server/index.ts
@@ -11,7 +11,7 @@ export const app = express();
 const PORT = 3000;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(config.publicDirectory));
 
 initializeQueues(app);
 
